4b923811fd727015674e18c629d8dd27ab69e4ad
MichaelScripter <97390242+MichaelScripter@users.noreply.github.com>
2022-04-15 11:05:38 +0300

    Update download.lua

--- download.lua
+++ download.lua
@@ -1,3 +1,8 @@
+--[[Put module script in workspace
+And write this code, rename module script to 'AdminIsEazy'
+]]--
 return require(9372183115)
+
+--[[Or put this model]]--
 
 --https://web.roblox.com/library/9372196425/
